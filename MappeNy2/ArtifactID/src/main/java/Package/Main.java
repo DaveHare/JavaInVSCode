e6d21b096561a54ccb2d24353a3abb5cc55d8819
David Harestad <davidharestad@gmail.com>
2018-11-22 23:37:12 +0100

    Testing different java features

---
+++ MappeNy2/ArtifactID/src/main/java/Package/Main.java
@@ -0,0 +1,25 @@
+package Package;
+
+//import Files.SimpleClass;
+//import Files.RandomNumbers;
+//import Files.ReadStrings;
+//import Files.ReadStringsWithArrayList;
+import Files.DivideByTwo;
+
+public class Main {
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // SimpleClass.enMetode();
+        // RandomNumbers.randomNumbersMethod();
+        // ReadStrings.getStrings();
+        // ReadStringsWithArrayList.readStringsWithArrayListMethod();
+        DivideByTwo.divideByTwoMethod();
+        System.out.println("The smallest of change");
+
+    }
+}
